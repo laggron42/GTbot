1d651511b0ce35c286c046bde5839b93330bfa15
retke <laggron42@gmail.com>
2021-12-10 20:09:53 +0100

    Add exceptions

--- gtbot/core/errors.py
+++ gtbot/core/errors.py
@@ -6,6 +6,15 @@ class GTException(Exception):
     pass
 
 
+class SilentError(GTException):
+    """
+    An error was already handled and should be silent,
+    but still propagate and cancel the stack call.
+    """
+
+    pass
+
+
 class NotFound(GTException):
     """
     The object you're looking for was not found.
@@ -19,4 +28,11 @@ class AlreadyPresent(GTException):
     You're trying to assign someone to solething he is already assigned to.
     """
 
+    pass
+
+
+class NotAllowed(GTException):
+    """
+    Someone is trying to perform an action he is not allowed to do.
+    """
     pass
